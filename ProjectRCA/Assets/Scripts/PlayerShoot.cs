6b2df3ac969509ac2209e40859a38e5013156ff8
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-01-07 13:58:19 -0800

    Added shoot script and a collect script

---
+++ ProjectRCA/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject prefab;
+    public float bulletSpeed = 10.0f;
+    public float bulletLifeTime = 1.0f;
+    public float shootDelay = 0.5f;
+    float timer = 0;
+    public int ammoCount = 10;
+    public int maxAmmo = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && timer > shootDelay && ammoCount > 0)
+            //if left click do something
+        {
+            timer = 0;
+            GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
+            ammoCount--;
+            if (x > 0)
+             {
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed;
+    
+             }
+            if (x < 0)
+            {
+                bullet.GetComponent<Rigidbody2D>().velocity = -transform.right * bulletSpeed;
+
+            }
+            Destroy(bullet, bulletLifeTime);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ammo" && ammoCount < 10)
+        {
+            ammoCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
