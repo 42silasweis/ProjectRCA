22cee8f51b7f5cf24c7241d21fd1571175b09133
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-01-08 13:02:10 -0800

    Created player ac

--- ProjectRCA/Assets/Scripts/PlayerShoot.cs
+++ ProjectRCA/Assets/Scripts/PlayerShoot.cs
@@ -11,6 +11,7 @@ public class PlayerShoot : MonoBehaviour
     float timer = 0;
     public int ammoCount = 10;
     public int maxAmmo = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,22 +23,34 @@ public class PlayerShoot : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         timer += Time.deltaTime;
-        if (Input.GetButton("Fire1") && timer > shootDelay && ammoCount > 0)
+        if (Input.GetButton("Fire2") && timer > shootDelay && ammoCount > 0)
             //if left click do something
         {
             timer = 0;
             GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
             ammoCount--;
+
             if (x > 0)
              {
+
             bullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed;
-    
+
              }
+
             if (x < 0)
             {
+
                 bullet.GetComponent<Rigidbody2D>().velocity = -transform.right * bulletSpeed;
 
             }
+
+            else if (x == 0)
+            {
+
+                bullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+
+            }
+
             Destroy(bullet, bulletLifeTime);
         }
     }
