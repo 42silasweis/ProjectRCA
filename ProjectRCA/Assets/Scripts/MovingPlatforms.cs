a15e6e1c2d75179585ef10b566e3cb1dc9d60ae9
Silas Weis <18.silas.weis@ksd.org>
2020-01-08 11:55:04 -0800

    removed Library folder

---
+++ ProjectRCA/Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public bool currentPos;
+    bool posMoved;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+    void MoveLeft()
+    {
+        if (currentPos == true && posMoved == false)
+        {
+            Vector3 v3 = transform.position;
+            v3.x += 20.0f;
+            transform.position = v3;
+        }
+    }
+}
