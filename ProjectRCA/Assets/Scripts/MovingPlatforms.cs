0c9cfcc229897d264f1007200e50e4fb50138a72
Silas Weis <18.silas.weis@ksd.org>
2020-01-09 12:08:58 -0800

    Fixed sprite flip on the new player movement script

--- ProjectRCA/Assets/Scripts/MovingPlatforms.cs
+++ ProjectRCA/Assets/Scripts/MovingPlatforms.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MovingPlatforms : MonoBehaviour
 {
+    public float moveDistX = 2.0f;
+    public float moveDistY = 2.0f;
+    public bool movingInYDir = false;
     public bool currentPos = true;
     bool posMoved = true;
     bool posMoved2 = true;
     public Transform plat1;
+    public float flipCoolDown = 1.0f;
+    float flipCoolDownTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -18,42 +23,66 @@ public class MovingPlatforms : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        flipCoolDownTimer += Time.deltaTime;
         if (currentPos == true)
         {
-            MoveLeft();
+            
+            MoveRight();
+            MoveUp();
             posMoved = true;
             posMoved2 = false;
         }
         if (currentPos == false)
         {
-            MoveRight();
+            
+            MoveLeft();
+            MoveDown();
             posMoved2 = true;
             posMoved = false;
         }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1) && collision.gameObject.tag == "Player" && !collision.isTrigger)
+        if (Input.GetKeyDown(KeyCode.Mouse1) && collision.gameObject.tag == "Player" && !collision.isTrigger && flipCoolDownTimer > flipCoolDown)
         {
             currentPos = !currentPos;
-        }
-    }
-    void MoveLeft()
-    {
-        if (posMoved == false)
-        {
-            Vector3 v3 = plat1.transform.position;
-            v3.x += 2.0f;
-            plat1.transform.position = v3;
+            flipCoolDownTimer = 0;
         }
     }
     void MoveRight()
     {
+        if (posMoved == false)
+        {
+            Vector3 v3 = plat1.transform.position;
+            v3.x -= moveDistX;
+            plat1.transform.position = v3;
+        }
+    }
+    void MoveLeft()
+    {
         if (posMoved2 == false)
         {
             Vector3 v3 = plat1.transform.position;
-            v3.x -= 2.0f;
+            v3.x += moveDistX;
             plat1.transform.position = v3;
+        }
+    }
+    void MoveUp()
+    {
+        if (posMoved == false)// && movingInYDir == true)
+        {
+            Vector3 v32 = plat1.transform.position;
+            v32.y += moveDistY;
+            plat1.transform.position = v32;
+        }
+    }
+    void MoveDown()
+    {
+        if (posMoved2 == false)// && movingInYDir == true)
+        {
+            Vector3 v32 = plat1.transform.position;
+            v32.y -= moveDistY;
+            plat1.transform.position = v32;
         }
     }
 }
